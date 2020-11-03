a07da619e4afbbca62b0e333ceeafb2e7c357b63
ahmedhabeeb73 <alnaib888@gmail.com>
2020-11-03 15:43:29 +0100

    add product listinf

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Container} from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Homescreen from './screens/HomeScreen';
 
 
 const App =() => {
@@ -10,8 +11,7 @@ const App =() => {
    <Header/>
    <main className="py-3">
      <Container>
-
-   <h1>Welcome to dundung shop</h1>
+     <Homescreen/>
      </Container>
    </main>
    <Footer/>
